caf03d2a9bfbca62e2c2ac965f64420b912ca0b2
Dhiraj Thakur <dimpybca@gmail.com>
2024-10-09 01:01:38 +0530

    sdhh

--- database/migrations/2024_08_04_063128_create_agents_info_table.php
+++ database/migrations/2024_08_04_063128_create_agents_info_table.php
@@ -20,20 +20,23 @@ class CreateAgentsInfoTable extends Migration
             $table->string('agent_address')->nullable();
             $table->string('agent_pincode')->nullable();
             $table->string('agent_city')->nullable();
+            $table->string('agent_country', 50)->nullable()->default('IN');
             $table->string('agent_issue_sign')->nullable();
             $table->string('agent_issue_loc_code')->nullable();
             $table->string('agent_issue_date')->nullable();
             $table->string('agent_account')->nullable();
             $table->integer('iata_agent_code')->nullable(); //7
             $table->integer('iata_agent_cass')->nullable(); //4
+            $table->string('agent_contact_person_phone', 20)->nullable();
+            $table->string('agent_contact_person_email', 50)->nullable();
             $table->string('office_airport')->nullable();
-            $table->string('office_function_designator')->nullable(); //2
-            $table->string('office_company_designator')->nullable();//2
+            $table->string('office_function_designator')->nullable();
+            $table->string('office_company_designator')->nullable();
             $table->string('office_file_reference')->nullable();
             $table->tinyInteger('participant')->default(0)->nullable();
             $table->string('participant_airport')->nullable();
             $table->string('prticipant_identifer')->nullable();
-            $table->string('participant_code')->nullable(); 
+            $table->string('participant_code')->nullable();
             $table->string('participant_file_reference')->nullable();
             $table->timestamps();
         });

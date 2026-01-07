b22ebe7615e25db29980afa067f3dd7e719fd7c0
Dhiraj Thakur <dimpybca@gmail.com>
2026-01-07 23:40:41 +0530

    jhg

--- config/common-data.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'descartes_username' => env('DESCARTES_USERNAME'),
-    'descartes_password' => env('DESCARTES_PASSWORD'),
-    'descartes_upload_url' => 'https://wwwtest.myvan.descartes.com/HttpUpload/SimpleUploadHandler.aspx',
-    // https://www.myvan.descartes.com/HttpUpload/SimpleUploadHandler.aspx
-    'python_path'=>'C:\\Python10\\python.exe'
-];

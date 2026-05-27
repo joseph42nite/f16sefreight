6381b02bfea10f9fa3ac090357b25f8de85ad6da
Hyphening <joseph.hyphening@gmail.com>
2026-05-27 14:21:32 +0530

    chore: ensure consistent favicon in ocr upload tool page

--- resources/views/tools/ocr/upload.blade.php
+++ resources/views/tools/ocr/upload.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>AWB OCR Extractor</title>
+  <link rel="icon" href="/media/assets/logos/favicon-white-64.png" type="image/png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
